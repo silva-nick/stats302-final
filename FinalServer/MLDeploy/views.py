ccb7a2c03cc758cd13871049c394f5fc6bf607cd
silva-nick <silva.d.nicholas@gmail.com>
2022-03-06 21:17:03 -0600

    final commit?

--- FinalServer/MLDeploy/views.py
+++ FinalServer/MLDeploy/views.py
@@ -10,9 +10,10 @@ from .serializer import TweetRequestSerializers
 import json
 import requests
 import pickle
+import requests
+import os
 
-import keras
-from keras.preprocessing.text import Tokenizer
+from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 import nltk
 nltk.download('stopwords')
@@ -29,7 +30,7 @@ class TweetView(viewsets.ModelViewSet):
 
 
 def create_header():
-    with open('./.twitter_cred.json') as f:
+    with open(os.path.join(os.path.dirname(__file__), './.twitter_cred.json')) as f:
         data = json.load(f)
 
         bearer = data["bearer_token"]
@@ -42,7 +43,7 @@ def create_header():
 
 def get_tweet(category):
     query_params = {
-        "query": category + " -is:retweet -has:images -has:emoji lang:en",
+        "query": category + " -is:retweet -has:images lang:en",
     }
 
     response = requests.get("https://api.twitter.com/2/tweets/search/recent", params=query_params, headers=create_header())
@@ -50,15 +51,15 @@ def get_tweet(category):
     if (response.status_code != 200):
         print("Error: status code not 200")
         print(response, query_params)
-        raise Exception()
+        raise Exception("ERROR", response.text)
 
     print(response.text)
 
-    return response.json()['data'][0]
+    return response.json()['data'][0]['text']
 
 def process_tweet(tweet, tweet_size):
     print(tweet)
-    
+
     stop_words = stopwords.words('english')
     stemmer = SnowballStemmer('english')
 
@@ -71,16 +72,18 @@ def process_tweet(tweet, tweet_size):
             tokens.append(stemmer.stem(token))
     tweet =  ' '.join(tokens)
 
-    tokenizer = pickle.load(open("../models/tokenizer.p", 'rb'))
+    cur_dir = os.path.dirname(__file__)
+    tokenizer = pickle.load(open(os.path.join(cur_dir, 'models\\tokenizer.p'), 'rb'))
 
     return pad_sequences(tokenizer.texts_to_sequences([tweet]), maxlen=tweet_size)
 
-def status(category):
+def status_(category):
     try:
-        tweet = process_tweet(get_tweet(category))
-        clf_model = keras.models.load_model('../models/final_model.h5')
-        return "positive" if clf_model.predict(tweet) else "negative"
-
+        tweet_raw = get_tweet(category)
+        tweet = process_tweet(tweet_raw, 40)
+        cur_dir = os.path.dirname(__file__)
+        clf_model = load_model(os.path.join(cur_dir, 'models\\final_model.h5'))
+        return ("Positive", tweet_raw, "#9cd6a3") if round(clf_model.predict(tweet)[0][0]) else ("Negative", tweet_raw, "#d69c9c")
     except ValueError as e: 
         return Response(e.args[0], status.HTTP_400_BAD_REQUEST) 
 
@@ -90,8 +93,8 @@ def FormView(request):
 
         if form.is_valid():
             category = form.cleaned_data['category']
-            result = status(category)
-            return render(request, 'status.html', {"data": result}) 
+            result, tweet, color = status_(category)
+            return render(request, 'status.html', {"status": result, "tweet":tweet, "color":color}) 
             
     form=TweetRequestForm()
     return render(request, 'form.html', {'form':form})
